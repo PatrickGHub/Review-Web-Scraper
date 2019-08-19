51cdef3b061849e241408c1c06d715bb55129064
Patrick Kelly <patrick@pkelly.co>
2019-08-19 20:20:18 +0100

    scrapes all manufacturers on carwow when function called

--- scrape.py
+++ scrape.py
@@ -4,23 +4,44 @@ import time
 import json
 from bs4 import BeautifulSoup
 
-url = 'https://www.carwow.co.uk/audi'
-response = requests.get(url)
-content = BeautifulSoup(response.content, 'html.parser')
+def carwow_scrape():
+    start_time = time.time()
+    manufacturer_url = 'https://www.carwow.co.uk/car-reviews'
+    manufacturer_response = requests.get(manufacturer_url)
+    manufacturer_content = BeautifulSoup(manufacturer_response.content, 'html.parser')
+    manufacturers = []
+    all_car_data = []
 
-models = content.find_all(class_= "card-generic")
-car_arr = []
+    for manufacturer in manufacturer_content.find_all(class_='multi-columns-list-reviews__brand-name'):
+        manufacturers.append(manufacturer.text.replace('\n', '').replace(' ', '-'))
 
-for car in models:
+    for manufacturer in manufacturers:
+        url = f'https://www.carwow.co.uk/{manufacturer}'
+        response = requests.get(url)
+        content = BeautifulSoup(response.content, 'html.parser')
 
-    carObject = {
-        'manufacturer': car.find(class_='card-generic__media-title').text.replace('\n', ''),
-        'model': car.find(class_='card-generic__media-subtitle').text.replace('\n', ''),
-        'rating': car.find(class_='wowscore-numerator').text if car.find(class_='wowscore-numerator') else '0',
-        'maxScore': car.find(class_='wowscore-denominator').text.split('/')[1] if car.find(class_='wowscore-denominator') else '10'
-    }
+        models = content.find_all(class_= "card-generic")
+        car_data = []
+        manufacturer_data = {
+            'manufacturer': manufacturer,
+            'models': car_data
+        }
 
-    car_arr.append(carObject)
+        for car in models:
+            carObject = {
+                'manufacturer': car.find(class_='card-generic__media-title').text.replace('\n', ''),
+                'model': car.find(class_='card-generic__media-subtitle').text.replace('\n', ''),
+                'rating': car.find(class_='wowscore-numerator').text if car.find(class_='wowscore-numerator') else '0',
+                'maxScore': car.find(class_='wowscore-denominator').text.split('/')[1] if car.find(class_='wowscore-denominator') else '10'
+            }
+            car_data.append(carObject)
 
-with open('carwowData.json', 'w') as outfile:
-    json.dump(car_arr, outfile, indent=4)
+        all_car_data.append(manufacturer_data)
+        time.sleep(1)
+
+    with open('carwowData.json', 'w') as outfile:
+        json.dump(all_car_data, outfile, indent=4)
+
+    print('* Scraped cw in %s seconds *' % round(time.time() - start_time, 2))
+
+carwow_scrape()
