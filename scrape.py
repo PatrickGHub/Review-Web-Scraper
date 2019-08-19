be86e0bf0cf4fd3d08335a76e348419b3c0525e3
Patrick Kelly <patrick@pkelly.co>
2019-08-19 19:48:11 +0100

    scrapes single manufacturer page on carwow

--- scrape.py
+++ scrape.py
@@ -1,11 +1,26 @@
 import requests
 import urllib.request
 import time
+import json
 from bs4 import BeautifulSoup
 
 url = 'https://www.carwow.co.uk/audi'
 response = requests.get(url)
+content = BeautifulSoup(response.content, 'html.parser')
 
-soup = BeautifulSoup(response.text, 'html.parser')
+models = content.find_all(class_= "card-generic")
+car_arr = []
 
-print(soup)
+for car in models:
+
+    carObject = {
+        'manufacturer': car.find(class_='card-generic__media-title').text.replace('\n', ''),
+        'model': car.find(class_='card-generic__media-subtitle').text.replace('\n', ''),
+        'rating': car.find(class_='wowscore-numerator').text if car.find(class_='wowscore-numerator') else '0',
+        'maxScore': car.find(class_='wowscore-denominator').text.split('/')[1] if car.find(class_='wowscore-denominator') else '10'
+    }
+
+    car_arr.append(carObject)
+
+with open('carwowData.json', 'w') as outfile:
+    json.dump(car_arr, outfile, indent=4)
